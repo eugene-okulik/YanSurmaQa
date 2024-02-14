15170e7c0e9cb3f7c5b3e42c29515fd3f226682e
Yan Surma <savethatfeeling@gmail.com>
2024-02-14 15:50:22 +0300

    update create_dict.py

--- homework/yan_surma/homework_4/create_dict.py
+++ homework/yan_surma/homework_4/create_dict.py
@@ -1,6 +1,6 @@
 my_tuple = ('apple', 'juice', 1.25, 144, True)
 my_list = [1, 2, 3, 'hello', None]
-my_dict = {1: 'value1', 'two': 2, 'green': 'apple', 'game': 'football', 121: 'oneTwoOne'}
+my_dict = {1: 'value1', 'two': 2, 3: 'apple', 4: 'football', 5: 'oneTwoOne'}
 my_set = {1, 14, 15, 'go', 'cool', 'red', 1007}
 
 final_dict = {'tuple': my_tuple, 'list': my_list, 'dict': my_dict, 'set': my_set}
@@ -11,7 +11,7 @@ my_list.append('last')
 my_list.pop(2)  # 'hello'
 
 my_dict['i am a tuple'] = 'random value'
-my_dict.pop(121)  # 121: 'oneTwoOne'
+my_dict.pop(5)  # 5: 'oneTwoOne'
 
 my_set.add('new element')
 my_set.remove('go')
