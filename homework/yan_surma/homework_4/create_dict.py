f4a21bcef8e6513ccee3d2d563d682808be7634f
Yan Surma <savethatfeeling@gmail.com>
2024-02-15 13:19:33 +0300

    fixed for deleted by index, fixed for dict element (should be a tuple)

--- homework/yan_surma/homework_4/create_dict.py
+++ homework/yan_surma/homework_4/create_dict.py
@@ -1,5 +1,5 @@
 my_tuple = ('apple', 'juice', 1.25, 144, True)
-my_list = [1, 2, 3, 'hello', None]
+my_list = [1, 'two', 'hello', 3, None]
 my_dict = {1: 'value1', 'two': 2, 3: 'apple', 4: 'football', 5: 'oneTwoOne'}
 my_set = {1, 14, 15, 'go', 'cool', 'red', 1007}
 
@@ -8,9 +8,10 @@ final_dict = {'tuple': my_tuple, 'list': my_list, 'dict': my_dict, 'set': my_set
 print(my_tuple[-1])
 
 my_list.append('last')
-my_list.pop(2)  # 'hello'
+popped = my_list.pop(2)  # Fixed: Удаляем по ключу
+print(popped)  # Выведем на печать удаленный элемент
 
-my_dict['i am a tuple'] = 'random value'
+my_dict['i am a tuple'] = ('cool', 2, 'black', 4, 'pink')  # Fixed: Добавляем tuple
 my_dict.pop(5)  # 5: 'oneTwoOne'
 
 my_set.add('new element')
