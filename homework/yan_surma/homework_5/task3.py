4a31b8c6bf6658c066074dcffce6b5db4402cf02
Yan Surma <savethatfeeling@gmail.com>
2024-02-17 02:56:41 +0300

    refactoring code for flake8

--- homework/yan_surma/homework_5/task3.py
+++ homework/yan_surma/homework_5/task3.py
@@ -2,4 +2,4 @@ students = ['Ivanov', 'Petrov', 'Sidorov']
 
 subjects = ['math', 'biology', 'geography']
 
-print(f'Students ' + ', '.join(students) + ' study these subjects: ' + ', '.join(subjects))
+print('Students ' + ', '.join(students) + ' study these subjects: ' + ', '.join(subjects))
