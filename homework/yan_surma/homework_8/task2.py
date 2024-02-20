58dd780316a0655fe6c4c6959aef5e25e5f83dba
Yan Surma <savethatfeeling@gmail.com>
2024-02-20 18:34:59 +0300

    refactor for task2

--- homework/yan_surma/homework_8/task2.py
+++ homework/yan_surma/homework_8/task2.py
@@ -1,14 +1,20 @@
 def fibonacci():
-    fib_list = []
     a, b = 0, 1
-    for i in range(1, 100001):
-        fib_list.append(a)
+    while True:
+        yield a
         a, b = b, a + b
-    return fib_list
 
 
-fibonacci_list = fibonacci()
-print(fibonacci_list[4])
-print(fibonacci_list[199])
-print(fibonacci_list[999])
-print(fibonacci_list[99999])
+f = fibonacci()
+
+
+def get_fibonacci_element(n):
+    for i in range(n + 1):  # добавим 1, т.к. индексация идет с 0
+        element = next(f)
+    return element
+
+
+print(get_fibonacci_element(5))
+print(get_fibonacci_element(200))
+print(get_fibonacci_element(1000))
+print(get_fibonacci_element(100000))
