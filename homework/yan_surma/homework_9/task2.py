031d7d841622bd92926200a2b6efd02884f2dde4
Yan Surma <savethatfeeling@gmail.com>
2024-02-22 11:14:48 +0300

    refactor for task2.py

--- homework/yan_surma/homework_9/task2.py
+++ homework/yan_surma/homework_9/task2.py
@@ -4,3 +4,7 @@ temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27,
 new_temp = filter(lambda x: x > 28, temperatures)
 new_temp = list(new_temp)
 print(new_temp)
+print(min(new_temp))
+print(max(new_temp))
+average_temp = sum(new_temp) / len(new_temp)
+print(round(average_temp, 2))  # Округлим до 2х знаков
