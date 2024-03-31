a290e2ff0a3f24b5fe6fc91c94a976af57de33bb
Yan Surma <savethatfeeling@gmail.com>
2024-04-01 00:53:33 +0300

    locust hw done!

---
+++ homework/yan_surma/hw_locust_1/data.py
@@ -0,0 +1,36 @@
+post_body = {
+    "name": "Test name",
+    "data": {
+        "year": 2000,
+        "price": 2000,
+        "CPU model": "Intel Core i9",
+        "Hard disk size": "1 TB"
+    }
+}
+create_body = {
+    "name": "Test name",
+    "data": {
+        "year": 2000,
+        "price": 2000,
+        "CPU model": "Intel Core i9",
+        "Hard disk size": "1 TB"
+    }
+}
+put_body = {
+    "name": "Apple MacBook M3 Pro",
+    "data": {
+        "year": 2019,
+        "price": 1849.99,
+        "CPU model": "Intel Core i9",
+        "Hard disk size": "1 TB"
+    }
+}
+patch_body = {
+    "name": "Apple MacBook M3 Pro",
+    "data": {
+        "year": 2019,
+        "price": 1849.99,
+        "CPU model": "Intel Core i9",
+        "Hard disk size": "1 TB"
+    }
+}
